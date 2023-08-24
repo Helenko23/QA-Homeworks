e29f3e4ad6903b6113ce66e65d71a0e13703953b
elenakaragyozova <el_karagiozova@yahoo.com>
2023-08-24 15:07:10 +0300

    Secondary commit of the Selenium Web Driver Homework to change the branch

---
+++ src/test/java/googlesearches/SearchAlphaAcademy_ExplicitWait_DiffBrowsers.java
@@ -0,0 +1,112 @@
+package googlesearches;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class SearchAlphaAcademy_ExplicitWait_DiffBrowsers {
+
+    public enum BrowserTypes {
+        CHROME,
+        CHROME_HEADLESS,
+        FIREFOX,
+        FIREFOX_HEADLESS,
+    }
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    private final String searchText = "Telerik Academy Alpha";
+    private final String searchResult = "IT Career Start in 6 Months - Telerik Academy Alpha";
+    private final String errorMessage = "Search result not found";
+
+    @BeforeAll
+    public static void driverSetUp() {
+        //Setup browser
+        driver = startBrowser(BrowserTypes.CHROME);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterAll
+    public static void driverClose() {
+        driver.close();
+    }
+
+    @Test
+    public void resultFound_when_SearchTermProvided_onBing() {
+        //Navigate to bing.com
+        driver.get("https://bing.com");
+
+        //Agree to consent
+        WebElement consentButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='bnp_btn_accept']")));
+        consentButton.click();
+
+        //Type text in search field
+        WebElement searchField = driver.findElement(By.xpath("//input[@id='sb_form_q']"));
+        searchField.sendKeys(searchText);
+
+        //Click search button
+        WebElement searchIconButton = driver.findElement(By.xpath("//label[@id='search_icon']"));
+        searchIconButton.click();
+
+        //Assert result found
+        WebElement firstResult = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ol[@id='b_results']/li[1]//h2/a[contains(., '" + searchText + "')]")));
+        Assertions.assertEquals(searchResult, firstResult.getText(), errorMessage);
+    }
+
+    @Test
+    public void resultFound_when_SearchTermProvided_onGoogle() {
+        //Navigate to bing.com
+        driver.get("https://google.com");
+
+        //Agree to consent
+        WebElement consentButton = driver.findElement(By.xpath("//button[@id='L2AGLb']"));
+        consentButton.click();
+
+        //Type text in search field
+        WebElement searchField = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+        searchField.sendKeys(searchText);
+
+        //Click search button
+        WebElement searchButton = wait.until(ExpectedConditions.visibilityOf(
+                driver.findElement(By.xpath("(//input[@type='submit' and @name='btnK'])[1]"))));
+        searchButton.click();
+
+        //Assert result found
+        WebElement firstResult = driver.findElement(By.xpath("(//a/h3)[1]"));
+
+        Assertions.assertEquals(searchResult, firstResult.getText(), errorMessage);
+    }
+
+    private static WebDriver startBrowser(BrowserTypes browserType) {
+        switch (browserType) {
+            case CHROME:
+                ChromeOptions chromeOptions = new ChromeOptions();
+                return new ChromeDriver(chromeOptions);
+            case CHROME_HEADLESS:
+                ChromeOptions chromeOptionsHeadless = new ChromeOptions();
+                chromeOptionsHeadless.addArguments("--headless");
+                return new ChromeDriver(chromeOptionsHeadless);
+            case FIREFOX:
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                return new FirefoxDriver(firefoxOptions);
+            case FIREFOX_HEADLESS:
+                FirefoxOptions firefoxOptionsHeadless = new FirefoxOptions();
+                firefoxOptionsHeadless.addArguments("--headless");
+                return new FirefoxDriver(firefoxOptionsHeadless);
+        }
+        return null;
+    }
+
+}
