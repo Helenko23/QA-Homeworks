8e1894b3c7a38a5d1cb9e10a82630552a5c2e51b
elenakaragyozova <el_karagiozova@yahoo.com>
2023-08-24 15:07:10 +0300

    Secondary commit of the Selenium Web Driver Homework to change the branch

---
+++ src/test/java/googlesearches/SearchAlphaAcademy_ImplicitWait.java
@@ -0,0 +1,74 @@
+package googlesearches;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+
+public class SearchAlphaAcademy_ImplicitWait {
+
+    private static WebDriver driver;
+    private final String searchText = "Telerik Academy Alpha";
+    private final String searchResult = "IT Career Start in 6 Months - Telerik Academy Alpha";
+    private final String errorMessage = "Search result not found";
+
+    @BeforeAll
+    public static void driverSetUp() {
+        //Setup browser
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterAll
+    public static void driverClose() {
+        driver.close();
+    }
+
+    @Test
+    public void resultFound_when_SearchTermProvided_onBing() {
+        //Navigate to bing.com
+        driver.get("https://bing.com");
+
+        //Agree to consent
+        WebElement consentButton = driver.findElement(By.xpath("//button[@id='bnp_btn_accept']"));
+        consentButton.click();
+
+        //Type text in search field
+        WebElement searchField = driver.findElement(By.xpath("//input[@id='sb_form_q']"));
+        searchField.sendKeys(searchText);
+
+        //Click search button
+        WebElement searchIconButton = driver.findElement(By.xpath("//label[@id='search_icon']"));
+        searchIconButton.click();
+
+        //Assert result found
+        WebElement firstResult = driver.findElement(By.xpath("//ol[@id='b_results']/li[1]//h2/a"));
+        Assertions.assertTrue(firstResult.getText().contains(searchResult), errorMessage);
+    }
+
+    @Test
+    public void resultFound_when_SearchTermProvided_onGoogle() {
+        //Navigate to bing.com
+        driver.get("https://google.com");
+
+        //Agree to consent
+        WebElement consentButton = driver.findElement(By.xpath("//button[@id='L2AGLb']"));
+        consentButton.click();
+
+        //Type text in search field
+        WebElement searchField = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+        searchField.sendKeys(searchText);
+
+        //Click search button
+        WebElement searchButton = driver.findElement(By.xpath("(//input[@type='submit' and @name='btnK'])[1]"));
+        searchButton.click();
+
+        //Assert result found
+        WebElement firstResult = driver.findElement(By.xpath("(//a/h3)[1]"));
+        Assertions.assertTrue(firstResult.getText().contains(searchResult), errorMessage);
+    }
+
+}
